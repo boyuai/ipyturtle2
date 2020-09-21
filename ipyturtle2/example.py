df2e2e1f52ac41ec8f9197cae41dfd4b5999ca7a
cqby <xiangyujames@outlook.com>
2020-09-21 18:32:55 +0800

    feat: filling

--- ipyturtle2/example.py
+++ ipyturtle2/example.py
@@ -8,11 +8,13 @@
 TODO: Add module docstring
 """
 
+from math import sqrt
 from ipywidgets import DOMWidget
 from traitlets import Unicode, Bool, Int, Float, Any
 from ._frontend import module_name, module_version
 import time
 import math
+
 
 class TurtleWidget(DOMWidget):
     """TODO: Add docstring here
@@ -24,23 +26,23 @@ class TurtleWidget(DOMWidget):
     _view_module = Unicode(module_name).tag(sync=True)
     _view_module_version = Unicode(module_version).tag(sync=True)
 
-    is_canvas_fixed = Bool(True).tag(sync=True)
-    canvas_width = Int(320).tag(sync=True)
-    canvas_height = Int(320).tag(sync=True)
-    is_turtle_on = Bool(True).tag(sync=True)
-    is_pen_on = True
+    _is_canvas_fixed = Bool(True).tag(sync=True)
+    _canvas_width = Int(320).tag(sync=True)
+    _canvas_height = Int(320).tag(sync=True)
+    _is_turtle_on = Bool(True).tag(sync=True)
+    _is_pen_on = True
+    _is_filling = Bool(False).tag(sync=True)
 
-    turtle_height = Int(20).tag(sync=True)
-    turtle_width = Int(10).tag(sync=True)
-    turtle_location_x = Float(0.0).tag(sync=True)
-    turtle_location_y = Float(0.0).tag(sync=True)
-    turtle_heading = Float(00.0).tag(sync=True)
-    turtle_heading_x = Float(1).tag(sync=True)
-    turtle_heading_y = Float(0).tag(sync=True)
+    _turtle_height = Int(15).tag(sync=True)
+    _turtle_width = Int(10).tag(sync=True)
+    _turtle_location_x = Float(0.0).tag(sync=True)
+    _turtle_location_y = Float(0.0).tag(sync=True)
+    _turtle_heading = Float(00.0).tag(sync=True)
 
-    commands = Any([]).tag(sync=True)
-    color = Unicode('black').tag(sync=True)
-    pen_size = Int(1).tag(sync=True)
+    _commands = Any([]).tag(sync=True)
+    _color = Unicode('black').tag(sync=True)
+    _fill_color = Unicode('black').tag(sync=True)
+    _pen_size = Int(1).tag(sync=True)
 
     _command_id = 0
 
@@ -52,180 +54,251 @@ class TurtleWidget(DOMWidget):
         self._reset()
 
     def _reset(self):
-        self.is_turtle_on = True
-        self.is_pen_on = True
-        self.turtle_location_x = 0
-        self.turtle_location_y = 0
-        self.turtle_heading = 90.0
-        self.turtle_heading_x = 0.0
-        self.turtle_heading_y = 1.0
-        self.color = 'black'
+        self._is_turtle_on = True
+        self._is_pen_on = True
+        self._turtle_location_x = 0
+        self._turtle_location_y = 0
+        self._turtle_heading = 90.0
+        self._color = 'black'
 
     def _incr_id(self):
         self._command_id += 1
         return self._command_id
 
+    def _push_command(self, command):
+        self._commands = self._commands + [{
+            "id": self._incr_id(),
+            "x": self._turtle_location_x,
+            "y": self._turtle_location_y,
+            "heading": self._turtle_heading,
+            "color": self._color,
+            "lineWidth": self._pen_size,
+            "fillColor": self._fill_color,
+            "isFilling": self._is_filling,
+            "isPenOn": self._is_pen_on,
+            "isTurtleOn": self._is_turtle_on,
+            ** command,
+        }]
+
+    def filling(self):
+        return self._is_filling
+
+    def begin_fill(self):
+        self._is_filling = True
+        self._push_command({
+            "type": "beginFill",
+        })
+
+    def end_fill(self):
+        self._is_filling = False
+        self._push_command({
+            "type": "endFill",
+        })
+
     def position(self):
-        return self.turtle_location_x, self.turtle_location_y
+        return self._turtle_location_x, self._turtle_location_y
+
+    def abs(_, pos):
+        return sqrt(pos[0] ** 2+pos[1] ** 2)
 
     def penup(self):
-        self.is_pen_on = False
+        self._is_pen_on = False
 
     def pendown(self):
-        self.is_pen_on = True
-    
+        self._is_pen_on = True
+
     def forward(self, distance):
-        start_x = self.turtle_location_x
-        start_y = self.turtle_location_y
-        self.turtle_location_x += self.turtle_heading_x * distance
-        self.turtle_location_y += self.turtle_heading_y * distance
-        if self.is_pen_on:
-            self.commands = self.commands + [{
-                "type": "line",
-                "x": start_x,
-                "y": start_y,
-                "dx": self.turtle_location_x,
-                "dy": self.turtle_location_y,
-                "color": self.color,
-                "id": self._incr_id(),
-                "lineWidth": self.pen_size,
-            }]
+        self._push_command({
+            "type": "line",
+            "distance": distance,
+        })
+        self._turtle_location_x += math.cos(
+            math.radians(self._turtle_heading)) * distance
+        self._turtle_location_y += math.sin(
+            math.radians(self._turtle_heading)) * distance
 
     def back(self, distance):
         self.forward(-distance)
-    
+
     def heading(self):
-        return self.turtle_heading
-    
-    def goto(self, x, y = None):
+        return self._turtle_heading
+
+    def goto(self, x, y=None):
         if y is None:
             y = x[1]
             x = x[0]
-        self.turtle_location_x = float(x)
-        self.turtle_location_y = float(y)
+        self._turtle_location_x = float(x)
+        self._turtle_location_y = float(y)
         self._update_turtle()
-    
-    def setpos(self, x, y = None):
+
+    def setpos(self, x, y=None):
         return self.goto(x, y)
 
-    def setposition(self, x, y = None):
+    def setposition(self, x, y=None):
         return self.goto(x, y)
 
     def left(self, degree=None):
         if degree is None:
             degree = 90
-        self.turtle_heading += degree
-        self.turtle_heading = self.turtle_heading % 360
-
-        hx = math.cos(math.radians(self.turtle_heading))
-        hy = math.sin(math.radians(self.turtle_heading))
-
-        self.turtle_heading_x = hx
-        self.turtle_heading_y = hy
-        self._update_turtle()
+        self._push_command({
+            "type": "left",
+            "degree": degree
+        })
+        self._turtle_heading += degree
+        self._turtle_heading = self._turtle_heading % 360
 
     def right(self, degree=None):
         if degree is None:
             degree = 90
-        self.left(-degree)
+        self._push_command({
+            "type": "right",
+            "degree": degree
+        })
+        self._turtle_heading -= degree
+        self._turtle_heading = self._turtle_heading % 360
 
     def isdown(self):
-        return self.is_pen_on
+        return self._is_pen_on
 
     def hideturtle(self):
-        self.is_turtle_on = False
+        self._is_turtle_on = False
         self._update_turtle()
 
     def showturtle(self):
-        self.is_turtle_on = True
+        self._is_turtle_on = True
         self._update_turtle()
 
     def isvisible(self):
-        return self.is_turtle_on
+        return self._is_turtle_on
 
     def reset(self):
         self._reset()
-        self.commands = self.commands + [{
-            "type": "clear",
-            "id": self._incr_id(),
-        }]
+        self._push_command({
+            "type": "reset",
+        })
 
-    def pencolor(self,r = -1, g = -1, b = -1):
+    def color(self, pen, fill):
+        self.pencolor(pen)
+        self.fillcolor(fill)
+
+    def pencolor(self, r=-1, g=-1, b=-1):
         if r == -1:
-            return self.color
+            return self._color
         elif type(r) == str:
-            self.color = r
+            self._color = r
         else:
-            self.color = 'rgb(' + str(r) + ', ' + str(g) + ', ' + str(b) + ')'
+            self._color = 'rgb(' + str(r) + ', ' + str(g) + ', ' + str(b) + ')'
+        self._update_turtle()
+
+    def fillcolor(self, r=-1, g=-1, b=-1):
+        if r == -1:
+            return self._fill_color
+        elif type(r) == str:
+            self._fill_color = r
+        else:
+            self._fill_color = 'rgb(' + str(r) + ', ' + \
+                str(g) + ', ' + str(b) + ')'
         self._update_turtle()
 
     def pensize(self, width=None):
         if width == None:
-            return self.pen_size
-        self.pen_size = width
-    
+            return self._pen_size
+        self._pen_size = width
+
     def _update_turtle(self):
-        self.commands = self.commands + [{
+        self._push_command({
             "type": "updateTurtle",
-            "id": self._incr_id(),
-        }]
+        })
 
     def write(self, text, move=False, align='left', font=("Arial", 8, "normal")):
-        self.commands = self.commands + [{
+        self._push_command({
             "type": "write",
-            "id": self._incr_id(),
             "text": text,
             "align": align,
             "font": font,
-            "x": self.turtle_location_x,
-            "y": self.turtle_location_y,
-            "color": self.color,
-        }]
-    
+        })
+
     def dot(self, size=None, color=None):
         if size is None:
-            if self.pen_size < 4:
-                size = self.pen_size + 4
+            if self._pen_size < 4:
+                size = self._pen_size + 4
             else:
-                size = self.pen_size * 2
-        if color is None:
-            color = self.color
-        self.commands = self.commands + [{
+                size = self._pen_size * 2
+        self._push_command({
             "type": "dot",
-            "id": self._incr_id(),
             "size": size,
-            "x": self.turtle_location_x,
-            "y": self.turtle_location_y,
-            "color": color,
-        }]
+            "dotColor": color,
+        })
 
     def circle(self, radius, extent=None):
         if extent is None:
             extent = 360
-        start = self.turtle_heading - 90
-        centerX = self.turtle_location_x + radius * math.cos(math.radians(start + 180))
-        centerY = self.turtle_location_y + radius * math.sin(math.radians(start  + 180))
-        self.commands = self.commands + [{
+        self._push_command({
             "type": "circle",
-            "id": self._incr_id(),
             "radius": radius,
-            "color": self.color,
-            "x": centerX,
-            "y": centerY,
-            "start": start,
             "extent": extent,
-            "lineWidth": self.pen_size,
-        }]
+        })
+        # 更新结束时的朝向和位置
+        start = self._turtle_heading - 90
+        centerX = self._turtle_location_x + radius * \
+            math.cos(math.radians(start + 180))
+        centerY = self._turtle_location_y + radius * \
+            math.sin(math.radians(start + 180))
         nextX = centerX + radius * math.cos(math.radians(extent + start))
         nextY = centerY + radius * math.sin(math.radians(extent + start))
-        self.goto(nextX, nextY)
-        self.left(extent)
+        nextHeading = self._turtle_heading + extent
+        self._turtle_location_x = nextX
+        self._turtle_location_y = nextY
+        self._turtle_heading = nextHeading
 
     def spiral_circle(self, steps, start_radius, radius_stride, angle_stride):
+        self._push_command({
+            "type": "spiralCircle",
+            "steps": steps,
+            "startRadius": start_radius,
+            "radiusStride": radius_stride,
+            "angleStride": angle_stride,
+        })
+        # 更新螺旋结束时的朝向和位置
+        nextX = self._turtle_location_x
+        nextY = self._turtle_location_y
+        nextHeading = self._turtle_heading
         for i in range(steps):
-            self.circle(start_radius + i * radius_stride, angle_stride)
+            radius = start_radius + i * radius_stride
+            start = nextHeading - 90
+            centerX = nextX + radius * math.cos(math.radians(start + 180))
+            centerY = nextY + radius * math.sin(math.radians(start + 180))
+            nextX = centerX + radius * \
+                math.cos(math.radians(angle_stride + start))
+            nextY = centerY + radius * \
+                math.sin(math.radians(angle_stride + start))
+            nextHeading += angle_stride
+        self._turtle_location_x = round(nextX, 10)
+        self._turtle_location_y = round(nextY, 10)
+        self._turtle_heading = nextHeading % 360
 
     def spiral_forward(self, steps, start_arc_length, arc_length_stride, angle_stride):
+        self._push_command({
+            "type": "spiralForward",
+            "steps": steps,
+            "startArcLength": start_arc_length,
+            "arcLengthStride": arc_length_stride,
+            "angleStride": angle_stride,
+        })
+        # 更新螺旋结束时的朝向和位置
+        # 模拟forward和left
+        nextX = self._turtle_location_x
+        nextY = self._turtle_location_y
+        nextHeading = self._turtle_heading
         for i in range(steps):
-            self.forward(start_arc_length + i * arc_length_stride)
-            self.left(angle_stride)
+            # forward
+            distance = start_arc_length + i * arc_length_stride
+            _turtle_heading_x = math.cos(math.radians(nextHeading))
+            _turtle_heading_y = math.sin(math.radians(nextHeading))
+            nextX += _turtle_heading_x * distance
+            nextY += _turtle_heading_y * distance
+            # left
+            nextHeading += angle_stride
+        self._turtle_location_x = round(nextX, 10)
+        self._turtle_location_y = round(nextY, 10)
+        self._turtle_heading = nextHeading % 360
